b64ab66271d57959327c168992208ecf6938b96d
Matsuel <matsuel0308@gmail.com>
2024-11-20 19:11:53 +0100

    Edit some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,13 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Button variant="secondary">Click Me!</Button>
         <Image
-          className="dark:invert"
+          className="invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Button variant="primary">
@@ -43,46 +33,38 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <CustomLink href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
+        <CustomLink href="https://github.com/Matsuel/Cloud" target="_blank">
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src="/github.svg"
+            alt="GitHub logo"
             width={16}
             height={16}
           />
-          Learn
+          GitHub
         </CustomLink>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <CustomLink href="https://matsuel.vercel.app" target="_blank">
           <Image
             aria-hidden
             src="/globe.svg"
-            alt="Globe icon"
+            alt="Personal website"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a>
+          Portfolio
+        </CustomLink>
+
+        <CustomLink href="https://www.linkedin.com/in/matheo-lang" target="_blank">
+          <Image
+            aria-hidden
+            src="/linkedin.svg"
+            alt="LinkedIn logo"
+            width={16}
+            height={16}
+          />
+          LinkedIn
+        </CustomLink>
+
       </footer>
     </div>
   );
