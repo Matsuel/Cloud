a6594d215b49d93c811a1d14659f48347ce39fc8
Matsuel <matsuel0308@gmail.com>
2024-11-20 19:26:03 +0100

    Add TODO

--- back-end/main.go
+++ back-end/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// PostgresSQL et Gorm pour la gestion des users, générer un uuid pour chaque user qu'in utilisera pour le dossier de l'utilisateur
+// API Hateoas comme ça on a déjà les actions possibles pour chaque fichier
+
 func main() {
 	fmt.Println("Hello, World!")
 }
